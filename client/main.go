5d1bc4c2a40125e1df25b0403263f5d601d65751
aeddi <antoine.e.b@gmail.com>
2018-07-13 22:43:45 +0200

    Signing/verifying functions using RSA-2048 added

--- client/main.go
+++ client/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	// "crypto"
+	"crypto"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha512"
@@ -27,10 +27,10 @@ func generateKeyPair() (*rsa.PrivateKey, *rsa.PublicKey) {
 }
 
 // Get user entry from stdin
-func getUserEntry() string {
+func getUserEntry(prompt string) string {
 
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter message: ")
+	fmt.Print(prompt)
 	text, _ := reader.ReadString('\n')
 
 	return strings.TrimSpace(text)
@@ -39,8 +39,7 @@ func getUserEntry() string {
 // Encrypt plain text using public key
 func encryptText(plainTextMsg string, clientPublicKey *rsa.PublicKey) string {
 
-	cipherTextMsg, err := rsa.EncryptOAEP(sha512.New(), rand.Reader,
-						clientPublicKey, []byte(plainTextMsg), []byte(""))
+	cipherTextMsg, err := rsa.EncryptOAEP(sha512.New(), rand.Reader, clientPublicKey, []byte(plainTextMsg), []byte(""))
 
 	if err != nil {
 		fmt.Println("Error during plain text encryption: ", err.Error)
@@ -53,8 +52,7 @@ func encryptText(plainTextMsg string, clientPublicKey *rsa.PublicKey) string {
 // Decrypt ciphertext using private key
 func decryptText(cipherTextMsg string, clientPrivateKey *rsa.PrivateKey) string {
 
-	decryptedTextMsg, err := rsa.DecryptOAEP(sha512.New(), rand.Reader,
-							clientPrivateKey, []byte(cipherTextMsg), []byte(""))
+	decryptedTextMsg, err := rsa.DecryptOAEP(sha512.New(), rand.Reader, clientPrivateKey, []byte(cipherTextMsg), []byte(""))
 
 	if err != nil {
 		fmt.Println("Error during plain text decryption: ", err.Error)
@@ -64,16 +62,59 @@ func decryptText(cipherTextMsg string, clientPrivateKey *rsa.PrivateKey) string 
 	return string(decryptedTextMsg)
 }
 
+// Sign authentification code using private key
+func signAuthCode(authCode string, clientPrivateKey *rsa.PrivateKey) []byte {
+
+	hash := crypto.SHA512
+	pssh := hash.New()
+	pssh.Write([]byte(authCode))
+	hashed := pssh.Sum(nil)
+
+	signature, err := rsa.SignPSS(rand.Reader, clientPrivateKey, hash, hashed, &rsa.PSSOptions{})
+
+	if err != nil {
+		fmt.Println("Error during authentification code signing: ", err.Error)
+		os.Exit(1)
+	}
+
+	return signature
+}
+
+// Verify authentification code using public key
+func verifAuthCode(authCode string, clientPublicKey *rsa.PublicKey, signature []byte) bool {
+
+	hash := crypto.SHA512
+	pssh := hash.New()
+	pssh.Write([]byte(authCode))
+	hashed := pssh.Sum(nil)
+
+	err := rsa.VerifyPSS(clientPublicKey, hash, hashed, signature, &rsa.PSSOptions{})
+
+	if err != nil {
+		return false
+	} else {
+		return true
+	}
+}
+
 func main() {
 
 	clientPrivateKey, clientPublicKey := generateKeyPair()
 
-	plainTextMsg := getUserEntry()
+	plainTextMsg := getUserEntry("Enter text to encrypt/decrypt: ")
 	cipherTextMsg := encryptText(plainTextMsg, clientPublicKey)
 	decryptedTextMsg := decryptText(cipherTextMsg, clientPrivateKey)
 
 	if plainTextMsg == decryptedTextMsg {
-		fmt.Println("It works!")
+		fmt.Println("Encryption/decryption works!")
 		fmt.Println(plainTextMsg, "==", decryptedTextMsg)
+	}
+
+	authCode := getUserEntry("Enter text to sign/verify: ")
+	signature := signAuthCode(authCode, clientPrivateKey)
+
+	if verifAuthCode(authCode, clientPublicKey, signature) {
+		fmt.Println("Siging/verifying works!")
+		fmt.Println(authCode)
 	}
 }
